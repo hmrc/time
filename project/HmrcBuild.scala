631c07a0956d534d4b1aa63ff63084932a204ba5
Charles Kubicek <charles.kubicek@digital.hmrc.gov.uk>
2015-04-20 10:37:56 +0100

    [NIJ] update to 1.4.0 snapshot

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -7,7 +7,7 @@ object HmrcBuild extends Build {
   import BuildDependencies._
   import uk.gov.hmrc.DefaultBuildSettings._
 
-  val versionApp = "1.3.0-SNAPSHOT"
+  val versionApp = "1.4.0-SNAPSHOT"
   val appName = "time"
 
   lazy val time = (project in file("."))
