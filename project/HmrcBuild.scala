f4795e5f2055a6bf65fc40fb08ab1450c9aab80c
Steve Smith <steve@alwaysagileconsulting.com>
2015-05-27 16:30:24 +0100

    MDTP-32 Sbt git versioning

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -1,28 +1,30 @@
 import sbt.Keys._
 import sbt._
-import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
 
 object HmrcBuild extends Build {
 
   import BuildDependencies._
   import uk.gov.hmrc.DefaultBuildSettings._
+  import uk.gov.hmrc.SbtAutoBuildPlugin
 
-  val versionApp = "1.5.0-SNAPSHOT"
   val appName = "time"
 
   lazy val time = (project in file("."))
-    .enablePlugins(SbtAutoBuildPlugin)
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
     .settings(
       name := "time",
       targetJvm := "jvm-1.7",
-      version := versionApp,
       libraryDependencies ++= Seq(
         Compile.nscalaTime,
         Test.scalaTest,
         Test.pegdown
       ),
       crossScalaVersions := Seq("2.11.6"),
-      ArtefactDescription()
+      developers := List(Developer("duncancrawford", "Duncan Crawford", "duncan.crawford@digital.hmrc.gov.uk", new URL("http://www.hmrc.gov.uk")),
+                         Developer("xnejp03", "Petr Nejedly", "petr.nejedly@digital.hmrc.gov.uk", new URL("http://www.hmrc.gov.uk")),
+                         Developer("alvarovilaplana", "Alvaro Vilaplana", "alvaro.vilaplana@digital.hmrc.gov.uk", new URL("http://www.hmrc.gov.uk")),
+                         Developer("davesammut", "Dave Sammut", "dave.sammut@digital.hmrc.gov.uk", new URL("http://www.hmrc.gov.uk")))
     )
 }
 
@@ -39,54 +41,4 @@ private object BuildDependencies {
 
   object Test extends Test("test")
 
-}
-
-object ArtefactDescription {
-
-  def apply() = Seq(
-    pomExtra := (<url>https://www.gov.uk/government/organisations/hm-revenue-customs</url>
-      <licenses>
-        <license>
-          <name>Apache 2</name>
-          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-        </license>
-      </licenses>
-      <scm>
-        <connection>scm:git@github.com:hmrc/time.git</connection>
-        <developerConnection>scm:git@github.com:hmrc/time.git</developerConnection>
-        <url>git@github.com:hmrc/time.git</url>
-      </scm>
-      <developers>
-        <developer>
-          <id>duncancrawford</id>
-          <name>Duncan Crawford</name>
-          <url>http://www.equalexperts.com</url>
-        </developer>
-        <developer>
-          <id>xnejp03</id>
-          <name>Petr Nejedly</name>
-          <url>http://www.equalexperts.com</url>
-        </developer>
-        <developer>
-          <id>alvarovilaplana</id>
-          <name>Alvaro Vilaplana</name>
-          <url>http://www.equalexperts.com</url>
-        </developer>
-        <developer>
-          <id>jakobgrunig</id>
-          <name>Jakob Grunig</name>
-          <url>http://www.equalexperts.com</url>
-        </developer>
-        <developer>
-          <id>vaughansharman</id>
-          <name>Vaughan Sharman</name>
-          <url>http://www.equalexperts.com</url>
-        </developer>
-        <developer>
-          <id>davesammut</id>
-          <name>Dave Sammut</name>
-          <url>http://www.equalexperts.com</url>
-        </developer>
-      </developers>)
-    )
 }
