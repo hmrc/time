f77cec664899241b33dda580a82ec052948bcb6e
Duncan Crawford <duncan@noughtsandones.co.uk>
2017-08-28 16:22:47 +0100

    increment lib versions

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -19,8 +19,8 @@ object HmrcBuild extends Build {
         Test.scalaTest,
         Test.pegdown
       ),
-      scalaVersion := "2.11.7",
-      crossScalaVersions := Seq("2.11.7"),
+      scalaVersion := "2.11.8",
+      crossScalaVersions := Seq("2.11.8"),
       developers := List(Developer("duncancrawford", "Duncan Crawford", "duncan.crawford@digital.hmrc.gov.uk", new URL("http://www.hmrc.gov.uk")),
                          Developer("xnejp03", "Petr Nejedly", "petr.nejedly@digital.hmrc.gov.uk", new URL("http://www.hmrc.gov.uk")),
                          Developer("alvarovilaplana", "Alvaro Vilaplana", "alvaro.vilaplana@digital.hmrc.gov.uk", new URL("http://www.hmrc.gov.uk")),
@@ -31,12 +31,12 @@ object HmrcBuild extends Build {
 private object BuildDependencies {
 
   object Compile {
-    val nscalaTime = "com.github.nscala-time" %% "nscala-time" % "2.2.0"
+    val nscalaTime = "com.github.nscala-time" %% "nscala-time" % "2.16.0"
   }
 
   sealed abstract class Test(scope: String) {
-    val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % scope
-    val pegdown = "org.pegdown" % "pegdown" % "1.5.0" % scope
+    val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3" % scope
+    val pegdown = "org.pegdown" % "pegdown" % "1.6.0" % scope
   }
 
   object Test extends Test("test")
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,4 +3,4 @@ resolvers += Resolver.url("hmrc-sbt-plugin-releases",
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.0.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "0.8.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "0.9.0")
